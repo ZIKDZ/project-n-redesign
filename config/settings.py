6441497a23af2f139ff6347620a115d26c7ed3e6
ILIAS <iliase.zaoui@gmail.com>
2026-04-21 11:08:13 +0100

    fix: switch whitenoise to CompressedStaticFilesStorage to fix collectstatic on Railway

--- config/settings.py
+++ config/settings.py
@@ -114,13 +114,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
-WHITENOISE_MANIFEST_STRICT = False
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
